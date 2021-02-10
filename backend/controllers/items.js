a7340a234ac53dcb5aaea5ea5a4c0acddba439cb
Jami Anttas <jami.anttas@edu.savonia.fi>
2021-02-10 15:04:02 +0200

    Backend tiedostorakenne pohjustettu

---
+++ backend/controllers/items.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import ItemModel from '../models/itemModel.js'
+
+//Get all items
+export const getAllItems = async (req, res) => {
+    try {
+        //Logic for get all items here
+    } catch (error) {
+        //error handling
+    }
+}
+//Add new Item
+export const addNewItem = async (req, res) => {
+    try {
+        //Logic for adding a new item
+    } catch (error) {
+        //error handling
+    }
+}
