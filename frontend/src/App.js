168d2985262367a2effcea21b7a83353eb8acab6
anttipieti <Antti.Pietikainen@edu.savonia.fi>
2021-03-01 12:06:30 +0200

    parempi sidebar - ei vielä valmis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 import Items from './Items.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const openHandler = () =>{
+    if(!sidebarOpen){
+      setSidebarOpen(true);
+    }
+    else{
+      setSidebarOpen(false);
+    }
+  }
+
+  
+
   return (
     <body>
       <div className="container-fluid">
         <header className="App-header">
           <h1>KYS-Varastonhallinta</h1>
         </header>
-        <nav class="col-md-3 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <Items />
-          </div>
-        </nav>
+        <Sidebar/>
+        <Toggle click = {openHandler}/>
       </div>
     </body>
   );
 }
 
 export default App;
+
+function Sidebar () {
+  return(
+      <div className="sidebar">
+          <h2>Tavarahaku</h2>
+          <button id="close">&times; SULJE</button>
+          <Items/>
+      </div>
+  )
+}
+
+function Toggle(props){
+  return(
+      <Fragment>
+          <button id="toggle" onClick = {props.click}>&#8801;</button>
+      </Fragment>
+  )
+}
