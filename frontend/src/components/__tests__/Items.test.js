7cc5310591374888ff3bd562b4f53c890233f13c
anttipieti <Antti.Pietikainen@edu.savonia.fi>
2021-04-22 09:56:15 +0300

    Testausta yritetty

--- frontend/src/components/__tests__/Items.test.js
+++ frontend/src/components/__tests__/Items.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {ReactDOM, unmountComponentAtNode} from 'react-dom';
+import { act } from "react-dom/test-utils";
 import Items from '../Items.js';
 
 import {render, cleanup} from '@testing-library/react';
@@ -20,16 +21,28 @@ afterEach(() => {
   container = null;
 });
 
+const mockCallback = jest.fn();
+
+
 it("renders fake data", async () => {
-    const fakeData = {    id: "213324", name: "Item1",    amount: "32",    location: "A123", category: "category1", image : "Kuva", number: "12345"  };  jest.spyOn(global, "fetch").mockImplementation(() =>    Promise.resolve({      json: () => Promise.resolve(fakeData)    })  );
+    const fakeData = {    id: "213324", name: "Item1",    amount: "32",    location: "A123", category: "category1", image : "Kuva", number: "12345"  
+  };  
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+        Promise.resolve({
+                json: () => Promise.resolve(fakeData)    
+              })  
+            );
     // Use the asynchronous version of act to apply resolved promises
     await act(async () => {
-      render(<Items id="123" />, container);
+      render(<Items selectedItem = {"213324"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
     });
   
-    expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
-    expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
-    expect(container.textContent).toContain(fakeUser.address);
+    //expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
+    //expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
+
+    //Kokeillaan sisältääkö renderöitävä komponentti luodun datan tiedot
+    //expect(container.textContent).toContain(fakeData.name);
+    expect(container.textContent).toContain("Valitse tavara");
   
     // remove the mock to ensure tests are completely isolated  
     global.fetch.mockRestore();});
