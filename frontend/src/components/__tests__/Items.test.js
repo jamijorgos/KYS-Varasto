69bf0bba77ae355a9df764aee98b349987e96e51
anttipieti <Antti.Pietikainen@edu.savonia.fi>
2021-04-26 20:49:24 +0300

    React testejä, kolmas testi ei toimi

--- frontend/src/components/__tests__/Items.test.js
+++ frontend/src/components/__tests__/Items.test.js
@@ -24,7 +24,7 @@ afterEach(() => {
 const mockCallback = jest.fn();
 
 
-test("renders fake data", async () => {
+test("renderöi annetun datan", async () => {
     const fakeData = {    id: "213324", name: "Item1",    amount: "32",    location: "A123", category: "category1", image : "Kuva", number: "12345"  
   };  
   jest.spyOn(global, "fetch").mockImplementation(() =>
@@ -39,33 +39,52 @@ test("renders fake data", async () => {
     });
     
     //Testataan ItemList funktiota. Renderöikö se annetun datan?
-
     //expect(container.querySelector("option").textContent).toContain("category1");
     expect(container.textContent).toContain("Item1");
-  
     // remove the mock to ensure tests are completely isolated  
     global.fetch.mockRestore();
   });
 
-  test("tyhjän palautus", async () => {
-    const fakeData = {    id: "", name: "",    amount: "",    location: "", category: "", image : "", number: ""  
-  };
+  
+test("tyhjä", async () => {
+  const fakeData = {  
+};  
+jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+              json: () => Promise.resolve(fakeData)    
+            })  
+          );
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<Items selectedItem = {"213324"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
+    
+  });
+  
+  //Testataan ItemList funktiota. Renderöikö se annetun datan?
+  //expect(container.querySelector("option").textContent).toContain("category1");
+  expect(container.textContent).not.toContain("Item1");
+  // remove the mock to ensure tests are completely isolated  
+  global.fetch.mockRestore();
+});
+
+  test("usean tavaran testausta", async () => {
+    const fakeData2 = 
+      {0:{id: "608", name: "lokiTest11", amount: "67", location: "B2", category: "test_category1", image: "juu", number: "464735", __v: 0},
+      1:{id: "12314", name: "lokiTest15", amount: "15", location: "B1", category: "test_category3", image: "joo", number: "46474", __v: 0}};
   jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
-                json: () => Promise.resolve(fakeData)    
+                json: () => Promise.resolve(fakeData2)    
               })  
             );
     // Use the asynchronous version of act to apply resolved promises
     await act(async () => {
-      render(<Items selectedItem = {"213324"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
+      render(<Items selectedItem = {"608"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
       
     });
     
     //Testataan ItemList funktiota. Renderöikö se annetun datan?
-
-    //expect(container.querySelector("option").textContent).toContain("category1");
-    expect(container.textContent).toContain("Item1");
-  
+    //expect(container.querySelector("option").textContent).toContain("");
+    expect(container.textContent).toContain("lokiTest11");
     // remove the mock to ensure tests are completely isolated  
     global.fetch.mockRestore();
   });
