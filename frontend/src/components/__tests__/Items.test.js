280773d76aefeb2356b999f7dc8348e5a5adafd1
anttipieti <Antti.Pietikainen@edu.savonia.fi>
2021-04-28 12:53:01 +0300

    testiä

--- frontend/src/components/__tests__/Items.test.js
+++ frontend/src/components/__tests__/Items.test.js
@@ -59,31 +59,30 @@ jest.spyOn(global, "fetch").mockImplementation(() =>
     render(<Items selectedItem = {"213324"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
     
   });
-  
-  //Testataan ItemList funktiota. Renderöikö se annetun datan?
-  //expect(container.querySelector("option").textContent).toContain("category1");
-  expect(container.textContent).not.toContain("Item1");
-  // remove the mock to ensure tests are completely isolated  
+  //Testataan ItemList funktiota. Renderöikö se tyhjän komponentin?
+  expect(container.textContent).not.toContain("Item1"); 
   global.fetch.mockRestore();
 });
 
   test("usean tavaran testausta", async () => {
-    const fakeData2 = 
-      {0:{id: "608", name: "lokiTest11", amount: "67", location: "B2", category: "test_category1", image: "juu", number: "464735", __v: 0},
-      1:{id: "12314", name: "lokiTest15", amount: "15", location: "B1", category: "test_category3", image: "joo", number: "46474", __v: 0}};
+    let fakeData2 = 
+      [JSON.stringify({id: "608", name: "lokiTest11", amount: "67", location: "B2", category: "test_category1", image: "juu", number: "464735", __v: 0}),
+      JSON.stringify({id: "12314", name: "lokiTest15", amount: "15", location: "B1", category: "test_category3", image: "joo", number: "46474", __v: 0})];
+      
+      const item1 = {id: "608", name: "lokiTest11", amount: "67", location: "B2", category: "test_category1", image: "juu", number: "464735", __v: 0};
+      const item2 = {id: "12314", name: "lokiTest15", amount: "15", location: "B1", category: "test_category3", image: "joo", number: "46474", __v: 0};
+      const fakeData3 = [item1,item2];
   jest.spyOn(global, "fetch").mockImplementation(() =>
         Promise.resolve({
-                json: () => Promise.resolve(fakeData2)    
+                json: () => Promise.all(fakeData2)    
               })  
             );
     // Use the asynchronous version of act to apply resolved promises
     await act(async () => {
       render(<Items selectedItem = {"608"} setSelectedItem = {mockCallback} setMapLocation={mockCallback} />, container);
-      
     });
     
     //Testataan ItemList funktiota. Renderöikö se annetun datan?
-    //expect(container.querySelector("option").textContent).toContain("");
     expect(container.textContent).toContain("lokiTest11");
     // remove the mock to ensure tests are completely isolated  
     global.fetch.mockRestore();
